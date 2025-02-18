0e5edb920aba25cb04ceae8c25b9dbbc82ba08d4
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-18 22:48:31 +0300

    Adding Pagination

--- task-manager-api/app/Http/Controllers/Api/TaskController.php
+++ task-manager-api/app/Http/Controllers/Api/TaskController.php
@@ -10,7 +10,7 @@ class TaskController extends Controller
 {
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::paginate(15);
         return TaskResource::collection($tasks);
     }
 
