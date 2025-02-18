4864b0514f3a98ed3972190186c038081ac2cb19
Zekariyas Tesfaye <99519232+ZekariyasT@users.noreply.github.com>
2025-02-18 22:38:04 +0300

    Adding Task Routes

--- task-manager-api/routes/api.php
+++ task-manager-api/routes/api.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    // We'll add our task routes here later
+    
+    Route::apiResource('tasks', TaskController::class);
 });
